5e8c839ddd4ad067939a9d529f6021dd67c0d2c0
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-18 14:40:50 +0530

    create member entity class and update dependencies

--- src/main/java/edu/icet/ecom/model/entity/MemberEntity.java
+++ src/main/java/edu/icet/ecom/model/entity/MemberEntity.java
@@ -1,7 +1,19 @@
 package edu.icet.ecom.model.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
 public class MemberEntity {
+    @Id
     private String id;
+
     private String name;
     private String nic;
     private String address;
