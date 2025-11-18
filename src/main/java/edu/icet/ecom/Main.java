745930b622ac3b5095b3d650f45ee94e9dbb22b3
Rishindu Yohan <rishinduyohan@gmail.com>
2025-11-18 10:15:00 +0530

    add dev branch

--- src/main/java/edu/icet/ecom/Main.java
+++ src/main/java/edu/icet/ecom/Main.java
@@ -2,6 +2,6 @@ package edu.icet.ecom;
 
 public class Main {
     public static void main(String[] args) {
-        IO.println("Initial project");
+        IO.println("Initial dev project");
     }
 }
